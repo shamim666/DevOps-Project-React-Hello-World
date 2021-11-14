bb53e4633ff3a4a372b01d374143428599f300d2
shamim <shamim.pstu@gmail.com>
2021-11-14 09:10:12 +0600

    front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  const [ cars , setCars ]  = useState([])
+  const nameRef = useRef();
+  const priceRef = useRef(); 
+
+  // get data from node server by using get method
+  useEffect( ()=>{
+    fetch(`http://localhost:5000/cars`)
+    .then(res =>res.json())
+    .then(data =>setCars(data))
+    
+  }, [])
+
+  const handleAddCars = (event) =>{
+    const name = nameRef.current.value 
+    const price = priceRef.current.value
+    const carInfo = { name: name , price : price }
+
+    // send data to backend server
+
+    fetch(`http://localhost:5000/cars` , {
+      method : 'post',
+      headers: {'content-type' : 'application/json'},
+      body: JSON.stringify(carInfo)  
+    })
+    .then()
+    event.preventDefault()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+        <h2>users :{cars.length} </h2>
+
+        <form onSubmit={handleAddCars}>
+          <input type = "text" ref={nameRef} placeholder="name" />
+          <br/>
+          <input type = "text" ref={priceRef} placeholder = "price" />
+          <br/>
+          <input type = "submit" value="submit"/>
+        </form>
+        <ul>
+          {cars.map(car => <li key={car.id}> name={car.name} price={car.price}</li>)}
+        </ul>
     </div>
   );
 }
